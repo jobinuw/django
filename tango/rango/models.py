42e8567c605952882a28c264b667c3988704c07f
jobinuw <jmmn84@hotmail.com>
2015-10-24 12:38:47 -0430

    django project

---
+++ tango/rango/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Artist(models.Model):
+  name = models.CharField("artist", max_length=50)
+  year_formed= models.PositiveIntegerField()
+  
+class Album(models.Model):
+  name = models.CharField("album", max_length=50)
+  artist= models.ForeignKey(Artist)
+  
+ # Creating and upodating data, Basic Queries, Custom Queries
+ 
+
